95dda7d09c715b0cd572331e3b975de85c3bb5d2
ZARIFMAC <uzbzarif@gmail.com>
2023-01-10 15:57:46 +0500

    spider;

---
+++ tutorial/tutorial/spiders/example.py
@@ -0,0 +1,18 @@
+import scrapy
+
+# Import Response class from scrapy.http module
+from scrapy.http import Response
+
+class ExampleSpider(scrapy.Spider):
+    name = 'example'
+    allowed_domains = ['example.com']
+    start_urls = ['https://asaxiy.uz/uz/product/brand/apple/page=2','http://python.org/']
+
+    def parse(self, response: Response):
+        print('Hello World!')
+        print(response)
+        page = response.url.split("/")[-2]
+        filename = f'{page}.html'
+        with open(filename, 'wb') as f:
+            f.write(response.body)
+        self.log(f'Saved file {filename}')
