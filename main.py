6d16d2191b06e7739acfae6f9f58add568e30198
ZARIFMAC <uzbzarif@gmail.com>
2023-01-11 15:17:11 +0500

    get text from selector

--- main.py
+++ main.py
@@ -1,6 +1,27 @@
 
 from scrapy.selector import Selector
-body = '<html><body><span>good</span></body></html>'
-print(Selector(text=body).xpath('//span/text()').get())
-# Using CSS
-print(Selector(text=body).css('span::text').get())
+body = """
+<html>
+<head>
+  <title>This is a title</title>
+  <meta content="text/html; charset=utf-8" http-equiv="content-type" />
+</head>
+<body>
+  <div>
+    <div>
+      <p>This is a paragraph.</p>
+      <p>Is this <a href="page2.html">a link</a>?</p>
+      <br />
+      Apparently.
+    </div>
+    <div class="second">
+      Nothing to add.
+      Except maybe this <a href="page3.html">other link</a>.
+      <!-- And this comment -->
+    </div>
+  </div>
+</body>
+</html>
+"""
+selector=Selector(text=body).xpath('/html/head/title/text()')
+print(selector[0].get())
